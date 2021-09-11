a9aaf9b333bb869530458644b32e284908589403
Massaru <massaruhagiopro@gmail.com>
2021-09-11 14:48:27 +0900

    update

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-function media() {
-    var num1 = 8;
-    var num2 = 5;
-    var media = (num1 + num2) / 2;
-    console.log(media);
+// programa para calculo de media -> nomes / nota1, nota2 dividido por 2 / aprovados e reprovados
+
+var nomes = ["Igor", "Yuuko", "Massaru"];
+var notas = [8.5, 7.0, 4.5];
+var notas2 = [2.0, 6.5, 3.5];
+
+function media(n1, n2) {
+    return (n1 + n2) / 2;
 }
-media();
+
+for (var index in nomes) {
+    console.log(nomes[index]);
+}
